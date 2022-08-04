d6893bfb01ae9667b4bc2203f10cbf9327e7ae1b
kilgur85 <103890819+kilgur85@users.noreply.github.com>
2022-08-04 20:07:42 +0300

    Добавил класс Tender

--- src/main/java/Tender.java
+++ src/main/java/Tender.java
@@ -1,11 +1,49 @@
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Tender {
     private BigDecimal priceForWork;
-    private int buildersInBrigade;
+    private List<Brigade>brigades;
 
+    public Tender(BigDecimal priceForWork, List<Brigade> brigades) {
+        this.priceForWork = priceForWork;
+        this.brigades = brigades;
+    }
+
+    public BigDecimal getPriceForWork() {
+        return priceForWork;
+    }
+
+    public void setPriceForWork(BigDecimal priceForWork) {
+        this.priceForWork = priceForWork;
+    }
+
+    public List<Brigade> getBrigades() {
+        return brigades;
+    }
+
+    public void setBrigades(List<Brigade> brigades) {
+        this.brigades = brigades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tender)) return false;
+        Tender tender = (Tender) o;
+        return Objects.equals(getPriceForWork(), tender.getPriceForWork()) && Objects.equals(getBrigades(), tender.getBrigades());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPriceForWork(), getBrigades());
+    }
+
+    @Override
+    public String toString() {
+        return "Tender{" +
+                "priceForWork=" + priceForWork +
+                ", brigades=" + brigades +
+                '}';
+    }
 }
